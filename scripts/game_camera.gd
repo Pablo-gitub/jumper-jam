fb13e3777452c43fdea032846d583dfbe9d69cfd
Paolo Pietrelli <pablos3339@gmail.com>
2025-03-17 05:15:20 +0100

    destroyer platforms

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,20 +1,36 @@
 extends Camera2D
 
+@onready var destroyer = $Destroyer
+@onready var destroyer_shape = $Destroyer/CollisionShape2D
+
 var player: Player = null
 var viewport_size
 
 func _ready() -> void:
 	viewport_size = get_viewport_rect().size
 	global_position.x = viewport_size.x / 2
+	#Definition of camera limits
 	limit_bottom = viewport_size.y
 	limit_left = 0
 	limit_right = viewport_size.x
+	
+	destroyer.position.y  = viewport_size.y
+	var rect_shape = RectangleShape2D.new()
+	var rect_shape_size = Vector2(viewport_size.x, 200)
+	rect_shape.set_size(rect_shape_size)
+	destroyer_shape.shape = rect_shape
 
 func _process(delta: float) -> void:
 	if player:
 		var limit_distance = 500
 		if limit_bottom > get_camera_target_to_player(player, limit_distance):
 			limit_bottom = get_camera_target_to_player(player, limit_distance)
+			
+	var overlapping_areas = destroyer.get_overlapping_areas()
+	if overlapping_areas.size() > 0:
+		for area in overlapping_areas:
+			if area is Platform:
+				area.queue_free()
 	
 func setup_camera(_player: Player):
 	if _player:
