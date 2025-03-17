d0c4bb792a08f88b5da0ecb8318b57cab882e547
Paolo Pietrelli <pablos3339@gmail.com>
2025-03-17 01:48:57 +0100

    Create platform level

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,6 +5,11 @@ var camera_scene = preload("res://scenes/game_camera.tscn")
 var platform_scene = preload("res://scenes/platform.tscn")
 var camera = null
 
+# Level gen variables
+var start_platform_y
+var y_distance_between_platforms = 200
+var level_size = 50
+
 func _ready() -> void:
 	camera = camera_scene.instantiate()
 	camera.setup_camera($Player)
@@ -17,6 +22,14 @@ func _ready() -> void:
 	for i in range(ground_layer_platform_count):
 		var ground_location = Vector2(i * platform_with, 0)
 		create_platform(ground_location)
+	#Generate the rest of the level
+	start_platform_y = viewport_size.y - (y_distance_between_platforms * 6)
+	for i in range(level_size):
+		var location: Vector2
+		var max_x_position = viewport_size.x-platform_with
+		location.x = randf_range( 0.0, max_x_position )
+		location.y = start_platform_y - (i * y_distance_between_platforms)
+		create_platform(location)
 	 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
